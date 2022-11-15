179f6998771f3fbaf51f8c9b48937ff507c0f5f4
dongyeop han <pig8776@naver.com>
2022-11-15 19:53:14 +0900

    first commit

--- baekjoon/Baekjoon10808.java
+++ baekjoon/Baekjoon10808.java
@@ -1,14 +1,14 @@
-package backjoon;
+package baekjoon;
 
 import java.io.*;
 
-public class Backjoon10808 {
+public class Baekjoon10808 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[26];  //알파벳 개수를 저장하는 배열
-        String s = br.readLine();  //단어 입력받기
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        String str = br.readLine();  //단어 입력받기
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
             arr[c - 97]++;  //개수 1씩 늘려주기
         }
         for (int i = 0; i < 26; i++) {
--- baekjoon/Baekjoon1406.java
+++ baekjoon/Baekjoon1406.java
@@ -1,2 +1,49 @@
-package baekjoon;public class Baekjoon1406 {
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon1406 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedList<String> editer = new LinkedList<String>();
+        String str[] = br.readLine().split("");
+
+        for (String s : str) {
+            editer.add(s);
+        }
+
+        ListIterator<String> iter = editer.listIterator(str.length);
+
+        int m = Integer.parseInt(br.readLine());
+
+        for (int i = 0 ; i < m ; i++) {
+            String command[] = br.readLine().split(" ");
+            if (command[0].equals("P")) {
+                iter.add(command[1]);
+            } else if (command[0].equals("L")) {
+                if (iter.hasPrevious()) {
+                    iter.previous();
+                } else {
+                    continue;
+                }
+            } else if (command[0].equals("D")) {
+                if (iter.hasNext()) {
+                    iter.next();
+                } else {
+                    continue;
+                }
+            } else if (command[0].equals("B")) {
+                if (iter.hasPrevious()) {
+                    iter.previous();
+                    iter.remove();
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        System.out.println(String.join("", editer));
+    }
 }
