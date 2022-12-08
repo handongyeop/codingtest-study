09843d87d3f5d947e50af83d6135ccc698a4ae19
dongyeop han <pig8776@naver.com>
2022-12-08 17:47:22 +0900

    22-12-08 study

---
+++ baekjoon/Baekjoon11047.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon11047 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int coin[] = new int[N];
+        int cnt = 0;
+
+        for (int i = 0 ; i < N ; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = N - 1 ; i >= 0 ; i--) {
+            if (K >= coin[i]) {
+              cnt += K / coin[i];
+              K = K % coin[i];
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
+
