c798f595a1978b68816515259b40ab244e94993b
dongyeop han <pig8776@naver.com>
2022-12-13 07:42:23 +0900

    22-12-13 study

---
+++ baekjoon/Baekjoon1920.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon1920 {
+    public static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0 ; i < M ; i++) {
+            if(Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+                sb.append(1).append('\n');
+            }
+            else {
+                sb.append(0).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
