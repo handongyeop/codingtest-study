07e25ac078712567630cdb6588f9c06582259a42
dongyeop han <pig8776@naver.com>
2022-12-30 10:33:17 +0900

    22-12-30 study

---
+++ baekjoon/Baekjoon14425.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon14425 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int count = 0;
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (set.contains(br.readLine())) count++;
+        }
+
+        System.out.print(count);
+    }
+}
